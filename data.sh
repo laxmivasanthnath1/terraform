54d40a223570f7f2014b558012a5886230217aa5
laxmi vasanth nath <164353907+laxmivasanthnath1@users.noreply.github.com>
2024-07-24 16:29:00 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo yum install git -y
+sudo yum install python3 -y
+sudo yum install python3-pip -y
+cd /home
+sudo git clone 
+cd /home/python-car-prediction/
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
