cd74e352f5fd8135a1a220119df5752c78888be3
laxmi vasanth nath <164353907+laxmivasanthnath1@users.noreply.github.com>
2024-07-24 16:30:38 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,7 +3,7 @@ sudo yum install git -y
 sudo yum install python3 -y
 sudo yum install python3-pip -y
 cd /home
-sudo git clone 
-cd /home/python-car-prediction/
+sudo git clone https://github.com/laxmivasanthnath1/MyFuel.git
+cd /home/MyFuel/
 pip3 install -r requirements.txt
 screen -m -d python3 app.py
